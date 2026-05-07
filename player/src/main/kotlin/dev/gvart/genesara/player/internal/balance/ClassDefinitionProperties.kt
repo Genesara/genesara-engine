00a73834b09f576fe143e1503ad94084fcc39b19
Vladlen Gladis <vladlen.gladis@deliveryhero.com>
2026-05-07 21:19:39 +0200

    chore: increase visibility to 3

--- player/src/main/kotlin/dev/gvart/genesara/player/internal/balance/ClassDefinitionProperties.kt
+++ player/src/main/kotlin/dev/gvart/genesara/player/internal/balance/ClassDefinitionProperties.kt
@@ -5,6 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "player")
 internal data class ClassDefinitionProperties(
-    val default: ClassProperties = ClassProperties(displayName = "Unassigned", sightRange = 1),
+    val default: ClassProperties = ClassProperties(displayName = "Unassigned", sightRange = 3),
     val classes: Map<AgentClass, ClassProperties> = emptyMap(),
 )
